6d2ab0a6fc2845cd9bc3e745cee4338899d207ee
Austin Terranova <austinterranova@mwc-120173.dhcp.missouri.edu>
2018-11-28 15:55:27 -0600

    fixed calender and added UI elements

--- Canlendar/Canlendar/CalendarVars.swift
+++ Canlendar/Canlendar/CalendarVars.swift
@@ -13,6 +13,6 @@ let date = Date()
 let calendar = Calendar.current
 
 let day = calendar.component(.day, from: date)
-let weekday = calendar.component(.weekday, from: date)
+var weekday = calendar.component(.weekday, from: date) - 1
 var month = calendar.component(.month, from: date) - 1
 var year = calendar.component(.year, from: date)
